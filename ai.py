4fd219fa56172a9718c2700bb80bbde8244342cd
rahulkundra2002 <126681161+rahulkundra2002@users.noreply.github.com>
2024-01-10 17:43:24 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,41 @@
+
+
+import speech_recognition as sr
+import pyttsx3
+
+r = sr.Recognizer()
+def reocord_text():
+    while(1):
+        try:
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2,duration=0.2)
+
+                audio2=r.listen(source2)
+
+                MyText = r.recognize_google(audio2)
+
+                return MyText
+        except sr.RequestError as e:
+            print("could not request as result{0}".format(e))
+
+        except sr.unknownValueError:
+            print("unkonwn error occured")
+
+
+    return
+
+
+def output_text(text):
+    f = open("output.Txt","a")
+    f.write(text)
+    f.write("\n")
+    f.close()
+
+    return
+
+while(1):
+    text=record_text()
+    output_text(text)
+
+
+    print("wrote text")
